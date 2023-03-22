4f79a7eab8b4ee6573479beec89ecd37ee8ccab3
Simon Joly <sim.joly@outlook.fr>
2023-03-22 10:23:03 +0100

    modif

---
+++ tp_3/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import { Navigate, Routes, Route } from "react-router-dom";
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+
+      <Routes path="/list"/>
+      <Route path="/list" element=/>
+      <Routes/>
+    </div>
+  )
+}
+
+export default App
