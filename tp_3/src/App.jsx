28d17bb3e7727e43adfde6dd7a9021d2f392b509
Simon Joly <sim.joly@outlook.fr>
2023-03-22 10:38:57 +0100

    router

--- tp_3/src/App.jsx
+++ tp_3/src/App.jsx
@@ -3,16 +3,25 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import { Navigate, Routes, Route } from "react-router-dom";
 import './App.css'
+import Nav from "./components/nav"
+import Homepage from "./components/homepage";
+import List from "./components/list";
+
 
 function App() {
   const [count, setCount] = useState(0)
 
+
   return (
     <div className="App">
+      <Nav>
+      </Nav>
+      <hr />
 
-      <Routes path="/list"/>
-      <Route path="/list" element=/>
-      <Routes/>
+      <Routes>
+        <Route path="/homepage" element={<Homepage />} />
+        <Route path="/list" element={<List />} />
+      </Routes>
     </div>
   )
 }
